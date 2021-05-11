7e22c7d5836c22ba8134998cc605731f2c8e6c66
Lauti Rozen <lautirozen@gmail.com>
2021-05-11 01:20:01 -0300

    localidades de prueba

---
+++ src/components/LocalidadesB.js
@@ -0,0 +1,26 @@
+export const LocalidadesB = [
+    {
+        id:1,
+        localidad: "Vicente López"
+    },
+    {
+        id:2,
+        localidad:"Florida",
+    },
+    {
+        id:3,
+        localidad:"Villa Martelli",
+    },
+    {
+        id:4,
+        localidad:"Florida Oeste",
+    },
+    {
+        id:5,
+        localidad:"Villa Ballester",
+    },
+    {
+        id:6,
+        localidad:"Quilmes",
+    },
+]
