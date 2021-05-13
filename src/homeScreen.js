bc94b00833994659154ede25e0a8f2b2ea3119d2
Lauti Rozen <lautirozen@gmail.com>
2021-05-12 23:12:47 -0300

    arreglo imagen de las cards + alineado al centro

--- src/homeScreen.js
+++ src/homeScreen.js
@@ -2,10 +2,8 @@ import React  from "react";
 import { makeStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Cartilla from './components/assets/cartilla.png';
 import Reviews from './components/assets/customerreview.png';
 import Typography from '@material-ui/core/Typography';
@@ -21,16 +19,15 @@ export default function Home() {
             // justifyContent:"center",
             // alignItems: "center"
         },
-        footer:{
-            marginTop:"15rem"
-        },
         root: {
-            margin: "5%",
+            marginTop:"3rem",
+            marginBottom:"5%",
             maxWidth: 345,
-            
+            marginLeft:"32%"
           },
           media: {
             height: 350,
+            backgroundSize:"contain"
           },
     }));
     const classes = useStyles();
@@ -39,46 +36,46 @@ export default function Home() {
         <div className={classes.Home}>
             {/* <div>En el home</div>
             <button onClick={() => history.push({pathname:'/Cartilla',state:"Vengo del home"})}>Ir a cartilla</button> */}
-            <div class= "row">
-            <Card className={classes.root}>
-      <CardActionArea onClick={() => history.push({pathname:'/Cartilla',state:"Vengo del home"})}>
-        <CardMedia
-          className={classes.media}
-          image={Cartilla}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Cartilla
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Busque profesionales, instituciones y actividades especializadas en el TEA
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.root}>
-      <CardActionArea onClick={() => history.push({pathname:'/Cartilla',state:"Vengo del home"})}>
-        <CardMedia
-          className={classes.media}
-          image={Reviews}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Compartir Experiencias
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Comparta o lea experiencias y datos acerca del TEA en nuestro foro comunitario
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            <div class="col-12 row mx-auto" >
+              <div class="col-md-6" >
+                <Card className={classes.root}>
+                  <CardActionArea onClick={() => history.push({pathname:'/Cartilla'})}>
+                    <CardMedia
+                      className={classes.media}
+                      image={Cartilla}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Cartilla
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        Busque profesionales, instituciones y actividades especializadas en el TEA
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </div>
+              <div class="col-md-4">  
+                <Card className={classes.root}>
+                  <CardActionArea onClick={() => history.push({pathname:'/Experiencias'})}>
+                    <CardMedia
+                      className={classes.media}
+                      image={Reviews}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Compartir Experiencias
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        Comparta o lea experiencias y datos acerca del TEA en nuestro foro comunitario
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </div>       
+          </div>         
+      <Footer />
     </div>
-            <div className={classes.footer}>
-                <Footer />
-            </div>
-        </div>
         
     );
 }
