2e146e4365ea70778bf4c32a90d975d12c283240
Lauti Rozen <lautirozen@gmail.com>
2021-05-13 01:26:50 -0300

    Copiar Link para compartir

--- src/detalleCartillaScreen.js
+++ src/detalleCartillaScreen.js
@@ -1,48 +1,45 @@
 import React, { useState} from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { useHistory} from "react-router-dom";
 import Footer from "./components/Footer";
-import { AirlineSeatIndividualSuiteRounded } from "@material-ui/icons";
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+
 
 export default function DetalleCartillaScreen(props) {
     const useStyles = makeStyles((theme) => ({
+        DetalleEntidad:{
+            backgroundColor:"#F2EFEB",
+            width:"100%",
+            height:"100%"
+        },
         Titulo:{
           fontFamily:"Garamond", 
           fontWeight:'bold',
           fontSize:"35px",
-          textAlign:"center",
-          marginTop:"10rem",
-          
+          textAlign:"left",
+          paddingLeft:"12.5rem",
+          paddingTop:"2rem"
         },
         Titulo1:{
             fontFamily:"Garamond", 
             fontWeight:'bold',
             fontSize:"50px",
             textAlign:"left",
-            marginTop:"7rem",
             color:"#7F3004",
-            
           },
           Titulo2:{
             fontSize:"25px",
             textAlign:"left",
-            marginTop:"5rem",
             textAlign:"left",
             marginLeft:"12rem",
           },
           footer:{
-              marginTop:"5rem"
+              marginTop:"6rem"
           },
           margin:{
-            color:"white",
-            backgroundColor:"#D27805",
-            '&:hover': {
-                backgroundColor:"#E89907",
-            }
-        },
-        margin1:{
             color:"white",
             backgroundColor:"#D27805",
             '&:hover': {
@@ -50,20 +47,35 @@ export default function DetalleCartillaScreen(props) {
             },
             marginLeft:"1rem"
         },
+        typography: {
+            padding: theme.spacing(2),
+            backgroundColor:"#F2EFEB"
+          },
       }));
-    const [entidad,setEntidad]=useState(props.location.state)
+    const [entidad]=useState(props.location.state)
     const history= useHistory();
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    const url = window.location.href
     return (
       <div className={classes.DetalleEntidad}>
-        <div className={classes.Titulo}>
-            {entidad.nombre} {entidad.apellido}   
-        </div>
         <div class="col-12 row mb-5">
             <div class="col-lg-3 col-md-8 col-sm-12 mt-5">
                 <img src={entidad.image} style={{marginLeft:"5rem"}}height="500px" class="img-fluid" alt="Responsive image" />
             </div>
             <div class="col-lg-7 col-md-4 col-sm-12 row">
+                <div className={classes.Titulo}>
+                    {entidad.nombre} {entidad.apellido}   
+                </div>
                 <div className={classes.Titulo2} >
                     Especialidad: {entidad.especialidad}<br />
                     Dirreción: {entidad.direccion} {entidad.piso}<br />
@@ -99,12 +111,31 @@ export default function DetalleCartillaScreen(props) {
             </div>
         </div>
         <div class="col-md-11 mx-auto">
-                <Button variant="outlined" size="medium" color="primary" className={classes.margin}>
+             <CopyToClipboard text={url}>
+                <Button variant="outlined" size="medium" color="primary" className={classes.margin} onClick={handleClick} >
                     Copiar Link
                 </Button>
-                <Button variant="outlined" size="medium" color="primary" className={classes.margin1} onClick={() => history.push({pathname:'/Experiencias'})}>
-                    Ver experiencias
-                </Button>
+            </CopyToClipboard>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                style={{marginTop:"1rem"}}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+                }}
+            >
+                <Typography className={classes.typography}>Se ha copiado el link al portapapeles!</Typography>
+            </Popover>
+                    <Button variant="outlined" size="medium" color="primary" className={classes.margin} onClick={() => history.push({pathname:'/Experiencias'})}>
+                        Ver experiencias
+                    </Button>
         </div>
         <div className={classes.footer}>
             <Footer />
